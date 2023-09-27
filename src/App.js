4e2e6be44aa526dfc901e2390ec5a9582964463e
Mariia Rozbitska <melissa180lily@gmail.com>
2023-09-27 19:45:04 +0300

    feat: add code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,110 @@
-import './App.css';
-import Records from './records.json';
+import './App.css';  
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const UserTable = () => {
+  const [users, setUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUsers(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleUserButtonClick = async (userId, type) => {
+    setSelectedUserId(userId);
+
+    if (type === 'todos') {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`);
+      const data = await response.json();
+      setTodos(data);
+    } else if (type === 'posts') {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    } else if (type === 'albums') {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums`);
+      const data = await response.json();
+      setAlbums(data);
+    }
+  };
+
+  const handleAlbumButtonClick = async (albumId) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`);
+    const data = await response.json();
+    setPhotos(data);
+  };
+
   return (
-    <div className="App">
-      {
-        Records && Records.map(record => {
-          return(
-            <div className="box" key = { record.id }>
-              { record.title } 
-              { record.content }
-              {record.person && record.person.map( data => {
-                return(
-                  <div key = { record.id }>
-                    {data.name}
-                    {data.age}
-                    {data.email}
-                  </div>
-                )
-              } ) }
-            </div>
-          )
-        })
-      }
+    <div>
+      <table id="userTable" width="100%">
+        <thead>
+          <tr>
+            <th colSpan="9">Users</th>
+          </tr>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>User name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Company</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.address.city}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+              <td>{user.company.name}</td>
+              <td>
+                <button
+                  data-user-id={user.id}
+                  className="todo-btn button"
+                  onClick={() => handleUserButtonClick(user.id, 'todos')}
+                >
+                  Todos
+                </button>
+                <button
+                  data-user-id={user.id}
+                  className="post-btn button"
+                  onClick={() => handleUserButtonClick(user.id, 'posts')}
+                >
+                  Posts
+                </button>
+                <button
+                  data-user-id={user.id}
+                  className="album-btn button"
+                  onClick={() => handleUserButtonClick(user.id, 'albums')}
+                >
+                  Albums
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Render other tables based on the state variables (todos, posts, etc.) */}
     </div>
   );
-}
+};
 
-export default App;
+export default UserTable;
